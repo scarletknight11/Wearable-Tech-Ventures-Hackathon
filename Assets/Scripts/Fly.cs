84f968e75e3165f19eaf33f78ecfce3ea48fb347
sanjit singh <ss3448@scarletmail.rutgers.edu>
2024-07-25 21:33:27 -0400

    implemented fly script. just need to test it

--- Assets/Scripts/Fly.cs
+++ Assets/Scripts/Fly.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fly : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Fly : MonoBehaviour {
+
+    public Transform head;
+
+    public OVRHand leftHand;
+    public OVRHand rightHand;
+
+    private bool isFlying = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isFlying)
+        {
+            isFlying = !isFlying;
+        }
+    
+            
+        if (isFlying)
+        {
+            Vector3 leftDir = leftHand.transform.position - head.position;
+            Vector3 rightDir = rightHand.transform.position - head.position;
+
+            Vector3 dir = leftDir + rightDir;
+
+            transform.position += (dir * .1f);
+        }
     }
 }
